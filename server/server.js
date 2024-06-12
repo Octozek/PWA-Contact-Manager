a0eaf79222f84607fd15bd65a54dcbf891ae87ae
Octozek <octozek@gmail.com>
2024-06-11 20:08:32 -0500

    Ensure server binds to correct port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Make sure to use process.env.PORT
+const PORT = process.env.PORT || 3000; // Ensure this line is correct
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
